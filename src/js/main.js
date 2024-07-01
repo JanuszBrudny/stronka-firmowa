4a74ac1dc9ea4d6d1e1f276b9f9e4449134f46d7
JanuszBrudny <139153218+JanuszBrudny@users.noreply.github.com>
2024-07-01 22:34:21 +0200

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,20 @@
 const navItems = document.querySelector(".nav-items");
 const menuBtn = document.querySelector(".menu-btn");
-
+const navLink = document.querySelectorAll(".link");
+const footerYear = document.querySelector(".footer-year");
 const handleNav = () => {
 	navItems.classList.toggle("active-nav");
 };
 
+navLink.forEach((link) => {
+	link.addEventListener("click", () => {
+		navItems.classList.remove("active-nav");
+	});
+});
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+handleCurrentYear();
 menuBtn.addEventListener("click", handleNav);
